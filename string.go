b3416ac242b21646d6a76901b9a751058497d767
joy <joy.lai@zg.com>
2019-04-18 15:53:23 +0800

    add

--- string.go
+++ string.go
@@ -10,6 +10,44 @@ type StringClass struct {
 
 var String = StringClass{}
 
+/**
+>7        前3后4中间4个*
+<=7 && >4 前2后2中间4个*
+<=4       报错
+ */
+func (this *StringClass) DesensitizeMobile(str string) string {
+	result := ``
+	length := len(str)
+	if length > 7 {
+		result = str[:3] + `****` + str[length - 4:]
+	} else if length <= 7 && length > 4 {
+		result = str[:2] + `****` + str[length - 2:]
+	} else {
+		panic(errors.New(`mobile length is too small`))
+	}
+	return result
+}
+
+/**
+@前字符串长度>3   前4 中4个* 后@后面所有
+@前字符串长度<=3  前@前面所有 中4个* 后@后面所有
+ */
+func (this *StringClass) DesensitizeEmail(str string) string {
+	result := ``
+	index := strings.Index(str, `@`)
+	if index == -1 {
+		panic(errors.New(`not email`))
+	}
+	preAt := str[:index]
+	if len(preAt) > 3 {
+		result = str[:4] + `****` + str[index:]
+	} else {
+		result = preAt + `****` + str[index:]
+	}
+	return result
+}
+
+
 func (this *StringClass) RemoveLast(str string, num int) string {
 	return str[:len(str)-num]
 }
--- string_test.go
+++ string_test.go
@@ -4,6 +4,31 @@ import (
 	"testing"
 )
 
-func TestStringClass_Test(t *testing.T) {
+func TestStringClass_DesensitizeMobile(t *testing.T) {
+	result := String.DesensitizeMobile(`18317042247`)
+	if result != `183****2247` {
+		t.Error(`18317042247 error`)
+	}
 
+	result1 := String.DesensitizeMobile(`4042247`)
+	if result1 != `40****47` {
+		t.Error(`4042247 error`)
+	}
+}
+
+func TestStringClass_DesensitizeEmail(t *testing.T) {
+	result := String.DesensitizeEmail(`pefish@qq.com`)
+	if result != `pefi****@qq.com` {
+		t.Error(`pefish@qq.com error`)
+	}
+
+	result1 := String.DesensitizeEmail(`abc@qq.com`)
+	if result1 != `abc****@qq.com` {
+		t.Error(`abc@qq.com error`)
+	}
+
+	result2 := String.DesensitizeEmail(`a@163.com`)
+	if result2 != `a****@163.com` {
+		t.Error(`a@163.com error`)
+	}
 }
